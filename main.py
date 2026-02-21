689b7e88ee1cf4019c678450282c02e5f40da6bc
naveend3v <naveenselvaraj1997@gmail.com>
2026-02-21 20:13:54 +0530

    middleware added

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from app.routes.issues import router as issues_router
+from app.middleware.timer import timing_middleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(issues_router)
-# @app.get('/health')
-# def get_health_check():
-#     return {'status':'ok'}
+app.middleware('http')(timing_middleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ['*'],
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
